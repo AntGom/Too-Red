943cac2d084ea9232df9cb96bfe48dc45da8db21
Antonio Gómez <antgomdgz@gmail.com>
2025-01-26 19:51:42 +0100

    error ruta cloudynaryConfig

--- api-too-red/config/multerConfig.js
+++ api-too-red/config/multerConfig.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "./cloudinaryConfig.js";
+import cloudinary from "./cloudinayConfig.js";
 
 //Configuración almacenamiento en Cloudinary
 const storage = new CloudinaryStorage({
