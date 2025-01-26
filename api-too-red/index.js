386efa0a2f55a2553a5230da07e1fe5a8d71b4e9
Antonio Gómez <antgomdgz@gmail.com>
2025-01-26 10:24:17 +0100

    express static

--- api-too-red/index.js
+++ api-too-red/index.js
@@ -9,7 +9,6 @@ import filterDeleted from "./middlewares/filterDeleted.js";
 import "./services/CleanUp.js";
 
 dotenv.config();
-
 conection();
 
 const app = express();
@@ -21,18 +20,17 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(filterDeleted);
+app.use("/uploads", express.static("uploads"));
 
+app.use(filterDeleted);
 app.use("/api/user", userRoutes);
 app.use("/api/publication", publicationRoutes);
 app.use("/api/follow", followRoutes);
 
 const PORT = process.env.PORT || 3900;
-
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
