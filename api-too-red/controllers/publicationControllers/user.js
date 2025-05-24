437a440b961e39d5faaed86c8715d956c9dc5cb4
Antonio Gómez <antgomdgz@gmail.com>
2025-05-24 15:06:11 +0200

    etiquetas en publications/user

--- api-too-red/controllers/publicationControllers/user.js
+++ api-too-red/controllers/publicationControllers/user.js
@@ -1,49 +1,53 @@
 import Publication from "../../models/publicationModel.js";
 
 const user = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const page = Math.max(1, parseInt(req.params.page) || 1);
-        const itemsPerPage = 5;
+  try {
+    const userId = req.params.id;
+    const page = Math.max(1, parseInt(req.params.page) || 1);
+    const itemsPerPage = 5;
 
-        const options = {
-            page,
-            limit: itemsPerPage,
-            sort: { createdAt: -1 },
-            populate: { path: "user", select: "_id name surname nick image" }
-        };
+    const options = {
+      page,
+      limit: itemsPerPage,
+      sort: { createdAt: -1 },
+      populate: [
+        { path: "user", select: "_id name surname nick image" },
+        { path: "tags", select: "_id name nick image" },
+        { path: "comments.user", select: "_id name nick image" },
+        { path: "likes.user", select: "_id name nick" },
+      ],
+    };
 
-        // Filtrar las publicaciones por el ID del usuario proporcionado con paginación
-        const publications = await Publication.paginate({ user: userId }, options);
+    // Filtrar las publicaciones por el ID del usuario proporcionado con paginación
+    const publications = await Publication.paginate({ user: userId }, options);
 
-        // Verificar si hay publicaciones
-        if (publications.totalDocs === 0) {
-            return res.status(200).json({
-                status: "success",
-                message: "No hay publicaciones para mostrar",
-                totalPages: 0, 
-                publications: []
-            });
-        }
-
-        return res.status(200).json({
-            status: "success",
-            message: "Publicaciones obtenidas correctamente",
-            totalArticles: publications.totalDocs,
-            totalPages: publications.totalPages,
-            currentPage: publications.page,
-            hasNextPage: publications.hasNextPage,
-            hasPrevPage: publications.hasPrevPage,
-            publications: publications.docs, 
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            message: "Error al obtener las publicaciones",
-            error: error.message,
-        });
+    // Verificar si hay publicaciones
+    if (publications.totalDocs === 0) {
+      return res.status(200).json({
+        status: "success",
+        message: "No hay publicaciones para mostrar",
+        totalPages: 0,
+        publications: [],
+      });
     }
+
+    return res.status(200).json({
+      status: "success",
+      message: "Publicaciones obtenidas correctamente",
+      totalArticles: publications.totalDocs,
+      totalPages: publications.totalPages,
+      currentPage: publications.page,
+      hasNextPage: publications.hasNextPage,
+      hasPrevPage: publications.hasPrevPage,
+      publications: publications.docs,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Error al obtener las publicaciones",
+      error: error.message,
+    });
+  }
 };
 
 export default user;
